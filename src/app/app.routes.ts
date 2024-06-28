0016b77e40d97e14f66723dfdfc13ff0d2ab88b3
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-06-28 12:10:05 -0300

    feat: add restaurant page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent, ListProyectsComponent, NotFoundComponent } from './pages';
+import { HomeComponent, ListProyectsComponent, NotFoundComponent, RestaurantesComponent } from './pages';
 
 export const routes: Routes = [
   {
     path: '', component: HomeComponent
+  },
+  {
+    path: 'restaurantes', component: RestaurantesComponent
   },
   {
     path: 'proyects/:category', component: ListProyectsComponent
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './not-found/not-found.component';
 export * from './home/home.component';
 export * from './list-proyects/list-proyects.component';
+export * from './restaurantes/restaurantes.component';
